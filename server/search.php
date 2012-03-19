494ef7c7f413a9a9cdee9694360b6608939ed0ef
kaihua liu <lkaihua@gmail.com>
2012-03-19 21:33:57 +0800

    Search.php will provide a json interface for front-end use. Add todos.

--- server/search.php
+++ server/search.php
@@ -1,9 +1,30 @@
 <?php
+/**
+ * Since search engines don't offer unlimited search api for normal users,
+ * This PHP converts the result page into a json object
+ * 
+ * By Kaihua Liu 2012/03/19
+ * 
+ * UNDER MIT LICENCE
+ */
+
+
+ 
+$engine = "baidu_mobile";
 
 $target_word = $_GET["word"];
 
 //$target_url = "http://www.baidu.com/s?ie=UTF-8&wd=".$target_word;
-$target_url = "http://m.baidu.com/s?word=".$target_word;
+
+$engines = array(
+	'baidu_mobile' => "http://m.baidu.com/s?word=", 
+	
+);
+
+
+$target_url = $engines[$engine].$target_word;
+
+// echo $target_url;
 
 $target_content = @file_get_contents($target_url);
 
@@ -15,5 +36,8 @@ $target_content = preg_replace('/<font[^>]*>([^<]*)<\/font>/i','<em>${1}</em>',$
 
 echo $target_content;
 
+
+//TODO: change the data format in to json
+//TODO: add google and bing
 ?>	
 
